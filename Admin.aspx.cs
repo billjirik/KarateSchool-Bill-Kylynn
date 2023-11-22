cbdb5fa43d9ae66903522e59efacb27449c13ece
kylynnerickson <125531228+kylynnerickson@users.noreply.github.com>
2023-11-21 18:16:23 -0600

    editing

--- Admin.aspx.cs
+++ Admin.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,10 +13,15 @@ namespace KarateSchool
     {
         string connString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\w_m_j\\OneDrive\\Desktop\\KarateSchool\\App_Data\\KarateSchool.mdf;Integrated Security=True;Connect Timeout=30";
         KarateSchoolDataContext dbcon;
+        private SqlConnection conn = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename=C:\\Users\\w_m_j\\OneDrive\\Desktop\\KarateSchool\\App_Data\\KarateSchool.mdf;Integrated Security = True; Connect Timeout = 30");
 
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                gvbind();
+            }
             dbcon = new KarateSchoolDataContext(connString);
 
             //select Mem records
@@ -70,6 +77,151 @@ namespace KarateSchool
             //show
             GridView2.DataSource= result;
             GridView2.DataBind();
+        }
+        protected void gvbind()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from detail", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                GridView1.DataSource = ds;
+                GridView1.DataBind();
+                int columncount = GridView1.Rows[0].Cells.Count;
+                GridView1.Rows[0].Cells.Clear();
+                GridView1.Rows[0].Cells.Add(new TableCell());
+                GridView1.Rows[0].Cells[0].ColumnSpan = columncount;
+                GridView1.Rows[0].Cells[0].Text = "No Records Found";
+            }
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            Label lbldeleteid = (Label)row.FindControl("lblID");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("delete FROM detail where id='" + Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()) + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind();
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            gvbind();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = (GridViewRow)GridView1.Rows[e.RowIndex];
+            Label lblID = (Label)row.FindControl("lblID");
+            //TextBox txtname=(TextBox)gr.cell[].control[];  
+            TextBox textName = (TextBox)row.Cells[0].Controls[0];
+            TextBox textadd = (TextBox)row.Cells[1].Controls[0];
+            TextBox textc = (TextBox)row.Cells[2].Controls[0];
+            //TextBox textadd = (TextBox)row.FindControl("txtadd");  
+            //TextBox textc = (TextBox)row.FindControl("txtc");  
+            GridView1.EditIndex = -1;
+            conn.Open();
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);  
+            SqlCommand cmd = new SqlCommand("update detail set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind();
+            //GridView1.DataBind();  
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            gvbind();
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            gvbind();
+        }
+
+
+        /////
+        ///
+
+        protected void gvbind2()
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * from detail", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+            }
+            else
+            {
+                ds.Tables[0].Rows.Add(ds.Tables[0].NewRow());
+                GridView2.DataSource = ds;
+                GridView2.DataBind();
+                int columncount = GridView2.Rows[0].Cells.Count;
+                GridView2.Rows[0].Cells.Clear();
+                GridView2.Rows[0].Cells.Add(new TableCell());
+                GridView2.Rows[0].Cells[0].ColumnSpan = columncount;
+                GridView2.Rows[0].Cells[0].Text = "No Records Found";
+            }
+        }
+        protected void GridView2_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)GridView2.Rows[e.RowIndex];
+            Label lbldeleteid = (Label)row.FindControl("lblID");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("delete FROM detail where id='" + Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value.ToString()) + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind2();
+        }
+        protected void GridView2_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView2.EditIndex = e.NewEditIndex;
+            gvbind2();
+        }
+        protected void GridView2_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int userid = Convert.ToInt32(GridView2.DataKeys[e.RowIndex].Value.ToString());
+            GridViewRow row = (GridViewRow)GridView2.Rows[e.RowIndex];
+            Label lblID = (Label)row.FindControl("lblID");
+            //TextBox txtname=(TextBox)gr.cell[].control[];  
+            TextBox textName = (TextBox)row.Cells[0].Controls[0];
+            TextBox textadd = (TextBox)row.Cells[1].Controls[0];
+            TextBox textc = (TextBox)row.Cells[2].Controls[0];
+            //TextBox textadd = (TextBox)row.FindControl("txtadd");  
+            //TextBox textc = (TextBox)row.FindControl("txtc");  
+            GridView2.EditIndex = -1;
+            conn.Open();
+            //SqlCommand cmd = new SqlCommand("SELECT * FROM detail", conn);  
+            SqlCommand cmd = new SqlCommand("update detail set name='" + textName.Text + "',address='" + textadd.Text + "',country='" + textc.Text + "'where id='" + userid + "'", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            gvbind2();
+            //GridView2.DataBind();  
+        }
+        protected void GridView2_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView2.PageIndex = e.NewPageIndex;
+            gvbind2();
+        }
+        protected void GridView2_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView2.EditIndex = -1;
+            gvbind2();
         }
     }
 }
